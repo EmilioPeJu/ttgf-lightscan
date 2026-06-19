49b8905973768ebe6e5fccb77ebe23ccca680e89
Emilio Perez Juarez <emiliopeju@gmail.com>
2026-06-19 18:22:31 +0100

    Wire everything together and add registers to control adquisition

--- hdl/spi_registers.vhd
+++ hdl/spi_registers.vhd
@@ -10,7 +10,16 @@ entity spi_registers is
         sck_i : in std_ulogic;
         rx_i : in std_ulogic;
         tx_o : out std_ulogic;
-        pos_i : in std_ulogic_vector(31 downto 0)
+        pos_i : in std_ulogic_vector(31 downto 0);
+        pos_valid_i : in std_ulogic;
+        error_event_i : in std_ulogic;
+        ntrig_o : out unsigned(31 downto 0);
+        trig_period_o : out unsigned(31 downto 0);
+        width_o : out unsigned(25 downto 0);
+        pos_req_o : out std_ulogic;
+        acq_start_o : out std_ulogic;
+        bissc_half_clk_period_o : out unsigned(7 downto 0);
+        bissc_n_rising_edges_o : out unsigned(5 downto 0)
     );
 end;
 
@@ -26,6 +35,10 @@ architecture rtl of spi_registers is
     signal cs : std_ulogic;
     signal rx : std_ulogic;
     signal bit_counter : unsigned(5 downto 0);
+    signal n_err : unsigned(7 downto 0);
+    signal n_over : unsigned(7 downto 0);
+    signal n_pos : unsigned(15 downto 0);
+    signal new_pos : std_ulogic;
 begin
     sync_sck_inst : entity work.sync_bit port map (
         clk_i => clk_i,
@@ -49,14 +62,40 @@ begin
     sck_fall <= sck_prev and not sck;
     sck_rise <= not sck_prev and sck;
 
+    count_proc : process (clk_i)
+    begin
+        if rising_edge(clk_i) then
+            if rst_i then
+                n_err <= (others => '0');
+                n_pos <= (others => '0');
+            else
+                if pos_valid_i then
+                    n_pos <= n_pos + 1;
+                end if;
+                if error_event_i then
+                    n_err <= n_err + 1;
+                end if;
+            end if;
+        end if;
+    end process;
+
     process (clk_i)
         variable next_datain : std_ulogic_vector(39 downto 0);
     begin
         if rising_edge(clk_i) then
             cs_prev <= cs;
             sck_prev <= sck;
+            pos_req_o <= '0';
+            acq_start_o <= '0';
             if rst_i then
                 dataout <= (others => '0');
+                new_pos <= '0';
+                n_over <= (others => '0');
+                ntrig_o <= (others => '0');
+                trig_period_o <= (others => '0');
+                width_o <= (others => '0');
+                bissc_half_clk_period_o <= (others => '0');
+                bissc_n_rising_edges_o <= (others => '0');
             elsif cs_fall then
                 bit_counter <= (others => '0');
             elsif sck_rise then
@@ -65,16 +104,47 @@ begin
                 datain <= next_datain;
                 if bit_counter = 7 then
                     case next_datain(7 downto 0) is
-                        when "00000001" => -- Test Identifier
-                            dataout <= x"42424242";
-                        when "00000010" => -- Command 2
+                        when x"00" => -- Magic ID
+                            dataout <= x"80000001";
+                        when x"01" => -- Counters
+                            dataout <= std_logic_vector(n_over & n_err & n_pos);
+                        when x"02" => -- Position
                             dataout <= pos_i;
+                            new_pos <= '0';
+                        when x"03" => -- Trigger period
+                            dataout <= std_logic_vector(trig_period_o);
+                        when x"04" => -- Number of triggers
+                            dataout <= std_logic_vector(ntrig_o);
+                        when x"05" => -- Pulse width
+                            dataout(31 downto 26) <= (others => '0');
+                            dataout(25 downto 0) <= std_logic_vector(width_o);
+                        when x"06" => -- BISS-C half clock period
+                            dataout(31 downto 8) <= (others => '0');
+                            dataout(7 downto 0) <=
+                                std_logic_vector(bissc_half_clk_period_o);
+                        when x"07" => -- BISS-C number of rising edges
+                            dataout(31 downto 6) <= (others => '0');
+                            dataout(5 downto 0) <=
+                                std_logic_vector(bissc_n_rising_edges_o);
                         when others =>
                             dataout <= (others => '0'); -- Default case
                     end case;
                 elsif bit_counter = 39 then
                     case next_datain(39 downto 32) is
-                        -- Placeholder to handle writes
+                        when x"83" => -- Trigger period
+                            trig_period_o <= unsigned(next_datain(31 downto 0));
+                        when x"84" => -- Number of triggers
+                            ntrig_o <= unsigned(next_datain(31 downto 0));
+                        when x"85" => -- Pulse width
+                            width_o <= unsigned(next_datain(25 downto 0));
+                        when x"86" => -- BISS-C half clock period
+                            bissc_half_clk_period_o <= unsigned(next_datain(7 downto 0));
+                        when x"87" => -- BISS-C number of rising edges
+                            bissc_n_rising_edges_o <= unsigned(next_datain(5 downto 0));
+                        when x"88" => -- Request position
+                            pos_req_o <= '1';
+                        when x"89" => -- Start acquisition
+                            acq_start_o <= '1';
                         when others =>
                             null;
                     end case;
@@ -82,6 +152,12 @@ begin
             elsif sck_fall then
                 dataout <= dataout(30 downto 0) & '0';
                 tx_o <= dataout(31);
+            end if;
+            if pos_valid_i then
+                new_pos <= '1';
+                if new_pos then
+                    n_over <= n_over + 1;
+                end if;
             end if;
         end if;
     end process;
--- hdl/tt_um_emiliopeju_lightscan_top.vhd
+++ hdl/tt_um_emiliopeju_lightscan_top.vhd
@@ -5,6 +5,7 @@ use ieee.numeric_std.all;
 entity tt_um_emiliopeju_lightscan_top is
     port (
         clk    : in  std_ulogic; -- clock
+
         ui_in  : in  std_ulogic_vector(7 downto 0); -- Dedicated inputs
         uo_out : out std_ulogic_vector(7 downto 0); -- Dedicated outputs
         uio_in : in  std_ulogic_vector(7 downto 0); -- IOs: Input path
@@ -17,25 +18,25 @@ entity tt_um_emiliopeju_lightscan_top is
 end;
 
 architecture rtl of tt_um_emiliopeju_lightscan_top is
-    signal pos : std_logic_vector(50 downto 0);
-    signal pos_valid : std_logic;
-    signal pos_err : std_logic;
-    signal rst : std_logic;
-    signal latched_pos : std_logic_vector(31 downto 0);
-    signal biss_data_sync : std_logic;
-    signal biss_data_sync_deglitched : std_logic;
+    signal pos : std_ulogic_vector(50 downto 0);
+    signal pos_valid : std_ulogic;
+    signal pos_err : std_ulogic;
+    signal pos_req : std_ulogic;
+    signal rst : std_ulogic;
+    signal biss_data_sync : std_ulogic;
+    signal biss_data_sync_deglitched : std_ulogic;
+    signal bissc_go : std_ulogic;
+    signal acq_start : std_ulogic;
+    signal trig : std_ulogic;
+    signal trig_period : unsigned(31 downto 0);
+    signal ntrig : unsigned(31 downto 0);
+    signal width : unsigned(25 downto 0);
+    signal bissc_half_clk_period : unsigned(7 downto 0);
+    signal bissc_n_rising_edges : unsigned(5 downto 0);
 begin
     uio_oe <= "00000001";
     rst <= not rst_n;
-
-    process (clk)
-    begin
-        if rising_edge(clk) then
-            if pos_valid then
-                latched_pos <= pos_err & pos(30 downto 0);
-            end if;
-        end if;
-    end process;
+    bissc_go <= pos_req or trig;
 
     reg_inst: entity work.spi_registers port map (
         clk_i => clk,
@@ -44,7 +45,28 @@ begin
         sck_i => ui_in(1),
         rx_i => ui_in(2),
         tx_o => uo_out(3),
-        pos_i => latched_pos
+        pos_i => pos(31 downto 0),
+        pos_valid_i => pos_valid,
+        error_event_i => pos_err,
+        ntrig_o => ntrig,
+        trig_period_o => trig_period,
+        width_o => width,
+        pos_req_o => pos_req,
+        acq_start_o => acq_start,
+        bissc_half_clk_period_o => bissc_half_clk_period,
+        bissc_n_rising_edges_o => bissc_n_rising_edges
+    );
+
+    pulsed_trigger_inst: entity work.pulsed_trigger port map (
+        clk_i => clk,
+        rst_i => rst,
+        go_i => acq_start,
+        busy_o => uo_out(0),
+        trig_period_i => trig_period,
+        ntrig_i => ntrig,
+        width_i => width,
+        trig_o => trig,
+        pulse_o => uo_out(1)
     );
 
     sync_biss_data_inst: entity work.sync_bit port map (
@@ -62,9 +84,9 @@ begin
     bissc_inst: entity work.bissc port map (
         clk_i => clk,
         rst_i => rst,
-        req_period_i => to_unsigned(1200000, 25),
-        half_clk_period_i => to_unsigned(5, 8),
-        n_rising_edges_i => to_unsigned(26, 6),
+        go_i => bissc_go,
+        half_clk_period_i => bissc_half_clk_period,
+        n_rising_edges_i => bissc_n_rising_edges,
         pos_o => pos,
         valid_o => pos_valid,
         err_o => pos_err,
